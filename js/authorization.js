787deffbcbc715c90c069538e4c673aff63aed0a
Ivan Nefagin <dfyz0r@list.ru>
2020-08-23 09:21:42 +0300

    feat(authorization): add user authorization

--- js/authorization.js
+++ js/authorization.js
@@ -1,1 +1,23 @@
 const authBtn = document.querySelector('.js-auth');
+
+authBtn.addEventListener('click', function () {
+  const loginName = prompt('Введите логин: ', 's4nyan0j'),
+    loginPass = prompt('Введите пароль: ', 'gr34tp4ss!337');
+
+  let isUserFound;
+  userLog.forEach(function (item) {
+    if (item.userName === loginName) isUserFound = item;
+  });
+
+  if (isUserFound) {
+    if (isUserFound.password === loginPass) {
+      greetingName.textContent = isUserFound.firstName;
+    } else {
+      greetingName.textContent = 'Аноним';
+      alert('Проверьте правильность написания пароля');
+    }
+  } else {
+    greetingName.textContent = 'Аноним';
+    alert('Пользователь не найден');
+  }
+});
